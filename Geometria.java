2167616fbcbd2c28673ad56e4d6e1d562c1e42d6
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-27 23:52:05 -0300

    Update Geometria.java

--- Geometria.java
+++ Geometria.java
@@ -12,7 +12,15 @@ public class Geometria {
   protected int alto;
   
   
-  public Geometria(int lado, double lado2, double radio, double altura, double cateto1, double cateto2, largo, ancho, alto){//metodo Constructor
-    
+  public Geometria(int lado, double lado2, double radio, double altura, double cateto1, double cateto2, int largo, int ancho, int alto){//metodo Constructor
+    this.lado=lado;
+    this.lado2.lado;
+    this.radio=radio;
+    this.altura=altura;
+    this.cateto1=cateto1;
+    this.cateto2=cateto2;
+    this.largo=largo;
+    this.ancho=ancho;
+    this.alto=alto;
   }
 }

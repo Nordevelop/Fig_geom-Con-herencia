52a0f5979775d0b08a996eea192946aa61a771d7
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-27 23:43:26 -0300

    Update Geometria.java

--- Geometria.java
+++ Geometria.java
@@ -5,6 +5,8 @@ public class Geometria {
   protected double lado2;
   protected double radio;
   protected double altura;
+  protected double cateto1;
+  protected double cateto2;
   
   
   public Geometria{//metodo Constructor

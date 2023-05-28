3f45abff24033075081200f914fd769ca197fd55
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2023-05-27 23:40:19 -0300

    Update Geometria.java

--- Geometria.java
+++ Geometria.java
@@ -1,7 +1,13 @@
 package misClases;
 
 public class Geometria {
+  protected int lado;
+  protected double lado2;
+  protected double radio;
+  protected double altura;
+  
   
   public Geometria{//metodo Constructor
+    
   }
 }
